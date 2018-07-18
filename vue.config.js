482d00b9ca88a0327a2d34feee66400650eeeaf3
savokiss <jaynaruto@qq.com>
2018-07-18 18:03:47 +0800

    feat: prerender

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
-module.exports = {
 
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: [
+          '/',
+          '/about'
+        ]
+      })
+    ]
+  }
 }
